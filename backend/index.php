91ae3a7d4b05480f390090a630b2940affd40aa6
karenmanzur <karenmanzur@users.noreply.github.com>
2019-04-01 16:09:39 -0500

    backend/usuarios - index

--- backend/index.php
+++ backend/index.php
@@ -17,26 +17,40 @@
 
       <div class="login-form">
         <div class="control-group">
-        <input type="text" class="login-field" value="" placeholder="Usuario" id="login-name">
-        <label class="login-field-icon fui-user" for="login-name"></label>
+        <input type="text" class="login-field" value="" placeholder="Usuario" id="inputEmail">
         </div>
 
         <div class="control-group">
-        <input type="password" class="login-field" value="" placeholder="Password" id="login-pass">
-        <label class="login-field-icon fui-lock" for="login-pass"></label>
+        <input type="password" class="login-field" value="" placeholder="Password" id="inputPassword">
         </div>
 
-        <a class="btn btn-primary btn-large btn-block" href="#">Entrar</a>
+        <button class="btn btn-primary btn-large btn-block" id="buttonSing" type="button">Entrar</a>
       </div>
     </div>
   </div>
 </body>
 
-
-
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<script>
+    $("#buttonSing").click(function(){
+      let correo = $("#inputEmail").val();
+      let password = $("#inputPassword").val();
+      let obj = {
+        "accion" : "login",
+        "correo" : correo,
+        "pswd" :  password
+      };
+      $.post("includes/_funciones.php", obj, function(r){
+        if(r == "1"){
+          window.location.replace("usuarios.php");
+        }else{
+          console.log("No concect");
+        }
+      });
+    });
+  </script>
 
 
 </body>
